158130d7c223524ee8da057abc7e2e5e39072449
Yash Kankane <yashkankane007@gmail.com>
2021-02-14 17:43:28 +0530

    Fix linting issue

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 def hello_world():
     """prints a simple message!"""
     return 'My First Capstone Project after rolling update!'
-    call()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
