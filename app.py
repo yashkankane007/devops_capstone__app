507f5fce13ab9d8fa50e04c9685357981b0f00c6
Yash Kankane <yashkankane007@gmail.com>
2021-02-14 17:39:59 +0530

    Lint fail commit

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     """prints a simple message!"""
-    return 'My First Capstone Project!'
+    return 'My First Capstone Project after rolling update!'
+    call()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
